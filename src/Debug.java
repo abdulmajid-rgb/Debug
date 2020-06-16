2ffc3fc19a0d1b802b14d63ca22e74e89bfc8eae
Abdulmejid <your-github-email>
2020-06-16 17:21:07 -0400

    Debug

---
+++ src/Debug.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Debug {
+    public static void main(String[] args) {
+            Scanner input =new Scanner(System.in);
+        System.out.println("please enter a score: ");
+            int testscore=input.nextInt();
+            char grade;
+
+
+        if(testscore >=90)
+
+            {
+                grade = 'A';
+            }
+        else if(testscore >=80)
+
+            {
+                grade = 'B';
+            }
+        else if(testscore >=70)
+
+            {
+                grade = 'C';
+            }
+        else if(testscore >=60)
+
+            {
+                grade = 'D';
+            }
+        else
+
+            {
+                grade = 'F';
+            }
+        System.out.println("Grade = "+grade);
+        }
+
+    }
+
+
+
